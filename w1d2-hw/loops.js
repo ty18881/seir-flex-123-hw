602f2ac041353c6daf491d7c997dbdc09be9eb36
Tara Young <ty18881@gmail.com>
2019-12-09 22:10:25 -0500

    finished loop practice

---
+++ w1d2-hw/loops.js
@@ -0,0 +1,34 @@
+
+/* Number 1 */
+
+for (let i=0; i<100; i++){
+    console.log(i);
+}
+
+
+/* Number 3 */
+
+for (let i=0; i<99; i+=2){
+    console.log(i);
+}
+
+/* Number 5 */
+
+for (let i=0; i<100; i++){
+    if (i%2!==0){
+        console.log(i);
+    }
+}
+/* Number 7 */
+
+for (let i=81; i>=26; i--){
+    console.log(i);
+}
+
+/* Number 9 */
+
+for (let i=3; i<= 90; i++){
+    if (i%3===0){
+        console.log(i);
+    }
+}
