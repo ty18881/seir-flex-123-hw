e99b11e3abf1d50df851cabcaa3836175b1bd051
Tara Young <ty18881@gmail.com>
2019-12-21 18:48:45 -0500

    crazy object complete

--- w03d03-hw/answers.js
+++ w03d03-hw/answers.js
@@ -43,3 +43,58 @@ console.log(taraYoung);
 
 console.log(taraYoung.hobby);
 
+/**Crazy Object */
+
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+  }
+
+
+//  1. Print "omg my mouth is burning" to the console
+// taco is an array that contains two anonymous objects.
+
+console.log(crazyObject.taco[1].salsa[5]);
+
+// 2.  "Pretty pretty prettayyyyy good"
+
+console.log(crazyObject.larry.quotes[0]);
+
+
+// 3. "Swearing at Larry and Jeff"
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+
+// 4.  "Chicken Teriyaki Boyyyyyy"
+console.log(crazyObject.larry.nicknames[1]);
+
+
+// 5.  The object the contains the name funkhauser
+console.log(crazyObject.larry.characters[1]);
+
+
+// 6.  Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
+
+crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk");
+
+console.log(crazyObject.larry.quotes);
