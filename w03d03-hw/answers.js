294323570ecbc79f297a0b8698ee453edcba2387
Tara Young <ty18881@gmail.com>
2019-12-21 18:08:29 -0500

    datatype refresher complete

---
+++ w03d03-hw/answers.js
@@ -0,0 +1,9 @@
+/**Datatypes question */
+
+// 1.  Light Switch:  Boolean
+// 2.  User's email address:  string
+// 3.  Spaceship with various properties:  Object
+// 4.  List of student names - Strings, Arrays
+// 5.  List of student names with location - Strings, Arrays, Objects
+// 6.  List of student names, location and list of favorite TV shows - Stringss, Array, Objects
+
