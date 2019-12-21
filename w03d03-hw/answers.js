88961f3169e57ddcb09472ce8b22304509066d38
Tara Young <ty18881@gmail.com>
2019-12-21 18:14:55 -0500

    took it easy

--- w03d03-hw/answers.js
+++ w03d03-hw/answers.js
@@ -7,3 +7,39 @@
 // 5.  List of student names with location - Strings, Arrays, Objects
 // 6.  List of student names, location and list of favorite TV shows - Stringss, Array, Objects
 
+
+/**Take it Easy */
+
+// 1.  Array of all the colors of the rainbow
+
+const rainbow = [
+    "Red",
+    "Orange",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Indigo",
+    "Violet"
+];
+
+console.log(rainbow);
+
+// 2.  Access Blue from the array
+
+console.log(rainbow[4]);
+
+// 3.  Object with information about me
+
+const taraYoung = {
+    favFood: "chocolate",
+    hobby:  "reading",
+    town:  "Brooklyn",
+    favDatatype: "number"
+};
+
+console.log(taraYoung);
+
+// 4.  Access the hobby 
+
+console.log(taraYoung.hobby);
+
