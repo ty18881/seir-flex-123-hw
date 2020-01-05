005ea9d7ccc76a018ad9b3c404dad8ecd72e1abe
Tara Young <ty18881@gmail.com>
2020-01-04 20:21:41 -0500

    common programming principles

---
+++ w04d1-hw/programming_fundamentals.js
@@ -0,0 +1,34 @@
+/**Homework:  Jan 4, 2020 */
+/** One Sentence about each of the below programming concepts */
+
+/**DRY*/
+// Don't repeat yourself - Avoid repeating similar code blocks across the program.  
+
+
+/** KISS */
+// Keep it simple stupid -  Write simple code, use simple constructs to ensure code is easier to troubleshoot and maintain.
+
+
+/** Avoid creating a YAGNI */
+// YAGNI = you aren't going to need it
+// Don't include program logic until you really need it.  
+// I struggle with this one the most.  For me, there's a push-pull between sequential logic and creating functions.
+// My tendency is to stick to sequential code but sometimes that's not the simplest or cleanest approach.
+
+/** Do the simplest thing that could possibly work */
+// Don't get fancy when something straightforward will get the job done.
+
+/** Don't make me think */
+// Don't require folks to use a Rosetta Stone to understand what your code is supposed to be doing.
+
+/** Write code for the maintainer */
+// Please comment your code so we don't spend most of our time figuring out what is supposed to be happening.
+
+/** Single responsibility principle */
+// I translate this as "specialization."  A block of code should do one thing.
+
+/** Avoid premature optimization */
+// Don't go for perfect when it won't give you significant improvement.
+
+/** Separation of concerns */
+// Functions should specialize and not have overlapping responsibilities.
