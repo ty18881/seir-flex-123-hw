14a63f414de7e7c374389a926253cc853a8f109a
Tara Young <ty18881@gmail.com>
2020-01-11 22:25:27 -0500

    dougie and pizza rat have fought

--- w04d3-hw/donut_adventure/adventure.js
+++ w04d3-hw/donut_adventure/adventure.js
@@ -13,9 +13,30 @@ class Hero {
          console.log(this.catchphrases[Math.floor(Math.random()*2)]);
     }
 
-    announceHealth = () => console.log(`Current Health: ${this.health}`);
+    announceHealth = () => console.log(`${this.name} Current Health: ${this.health}`);
 
-    fight = () => console.log(`I'm ready to rumble!`);
+// 1.  Upgraded fight method - accesses weapons and logs hitpoints.
+    // fight = () => console.log(`I'm ready to rumble!`);
+
+    selectWeapon = (weapons) => {
+        let weaponKeys = Object.keys(weapons);
+        console.log(`Keys for weapons object: ${weaponKeys}`);
+        return weapons[weaponKeys[Math.floor(Math.random()*2)]];
+    }
+// 3.  Third upgrade to fight method - update enemy health and display revised health value.
+
+    fight = (enemy) => {
+        // console.log(this.weapons);
+     
+        // random weapon selection to keep things interesting.
+
+        let weaponHit = this.selectWeapon(this.weapons);
+        console.log(`Oooh burn:  Hit you with ${weaponHit}`);
+        enemy.health-= weaponHit;
+        console.log(`our enemy: ${enemy.name} new health: ${enemy.health}`);
+    }
+
+    
 
 }
 
@@ -33,9 +54,42 @@ class Enemy {
          console.log(this.catchphrases[Math.floor(Math.random()*2)]);
     }
 
-    announceHealth = () => console.log(`Current Health: ${this.health}`);
-    fight = () => console.log(`I'm ready to rumble!`);
+    announceHealth = () => console.log(`${this.name} Current Health: ${this.health}`);
 
+    selectWeapon = (weapons) => {
+        let weaponKeys = Object.keys(weapons);
+        console.log(`Keys for weapons object: ${weaponKeys}`);
+        return weapons[weaponKeys[Math.floor(Math.random()*2)]];
+    }
+
+    // 1. Upgrading Fight method to access weapons and log health.
+
+    // fight = () => console.log(`I'm ready to rumble!`);
+
+    // fight = () => {
+    //     console.log(this.weapons.pepperoniStars);
+    //     console.log(`Hitpoints: ${this.health}`);
+    // }
+
+// 2. Second upgrade to fight method - accepts an argument.
+// fight = (enemy) => {
+//         console.log(this.weapons);
+//         console.log(`Hitpoints: ${this.health}`);
+//         console.log(`our enemy: ${enemy.name}`);
+//     }
+
+
+// 3.  Third upgrade to fight method - update enemy health and display revised health value.
+    fight = (enemy) => {
+        // console.log(this.weapons);
+    
+        // random weapon selection to keep things interesting.
+
+        let weaponHit = this.selectWeapon(this.weapons);
+        console.log(`Oooh burn:  Hit you with ${weaponHit}`);
+        enemy.health-= weaponHit;
+        console.log(`our enemy: ${enemy.name} new health: ${enemy.health}`);
+    }
 }
 let dougie = new Hero("Dougie");
 
@@ -56,4 +110,25 @@ const warOfWords = () => {
     pizzaRat.announceHealth();
 }
 
-warOfWords();
+// dougie and pizzaRat jawbone in the street
+// warOfWords();
+
+
+// Things have escalated and now our Hero and Enemy begin to fight for real.
+
+// testing dougie's upgraded fight method.
+
+// dougie.fight();
+
+// testing pizzaRat's upgraded fight method.
+
+// pizzaRat.fight();
+
+// testing second upgrade to fight method
+
+dougie.fight(pizzaRat);
+pizzaRat.announceHealth();
+
+pizzaRat.fight(dougie);
+dougie.announceHealth();
+
