4b5da2bc710b93bbbc1de99da1cba8d462e3e504
Tara Young <ty18881@gmail.com>
2020-01-18 17:38:36 -0500

    Commit 1 - Added first element

--- w05d3-hw/hogwarts/app.js
+++ w05d3-hw/hogwarts/app.js
@@ -1,2 +1,15 @@
 
-  
+  $(() => {
+/** YEAR 1:  Creating an H1 element on the page */
+
+let $container = $("#container");
+console.log($container);
+
+let $h1 = $("<h1>").text("Hogwarts");
+
+console.log($h1);
+
+// append the h1 to the original so it shows up.
+
+    $container.append($h1);
+  });
