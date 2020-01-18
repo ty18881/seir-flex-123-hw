83bb583f3a4e5090beb0ec8ea8434966fc3dc071
Tara Young <ty18881@gmail.com>
2020-01-18 17:50:52 -0500

    Commit 2 - I added more elements.  My power grows with my dedication and hard work.

--- w05d3-hw/hogwarts/app.js
+++ w05d3-hw/hogwarts/app.js
@@ -12,4 +12,15 @@ console.log($h1);
 // append the h1 to the original so it shows up.
 
     $container.append($h1);
+
+    /** YEAR 2:  Adding more h elements and special font */
+
+    let $name = $("<h2>").text("Tara");
+
+    let $house = $("<h3>").text("Ravenclaw");
+    let $petName = $("<h4>").addClass("cat").text("Stripes");
+
+    let $wand = $("<h4>").text("Birch Wand with Phoenix Feather Core");
+
+    $container.append($name, $house, $petName, $wand);
   });
