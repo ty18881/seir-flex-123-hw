2835904c2d1da6e9ddb4429b1c3f8bd0023cb95f
Tara Young <ty18881@gmail.com>
2020-01-18 23:05:41 -0500

    Commit 4 - Adding order is always more effort than causing chaos

--- w05d3-hw/hogwarts/app.js
+++ w05d3-hw/hogwarts/app.js
@@ -1,9 +1,14 @@
 
 const myStuff = ["butter beer", "invisibility cloak", "magic map", "time turner", "leash","Bertie Bott's Every Flavor [Jelly] Beans"];
 
+const myClasses = [ 
+    {day: "Monday", classes: ["Transfiguration", "Charms"]},
+    {day: "Tuesday", classes: ["Potions", "History of Magic"]},
+    {day: "Wednesday", classes: ["Defense Against the Dark Arts", "Astronomy"]},
+    {day: "Thursday", classes: ["Herbology", "Divination"]},
+    {day: "Friday", classes: ["Arithmancy" , "Muggle Studies"]}];
 
-
-
+const tableHeaders = [ "Day", "Classes"];
 
   $(() => {
 
@@ -13,6 +18,7 @@ const myStuff = ["butter beer", "invisibility cloak", "magic map", "time turner"
 
     const addToTrunk = (element) => {
         // create new list item from the array 
+
         let $li = $("<li>").text(element);
     
         // add class to the secret elements {cloak, map, time turner}
@@ -26,13 +32,54 @@ const myStuff = ["butter beer", "invisibility cloak", "magic map", "time turner"
             case "leash":
                 $li.addClass("cat");
                 break;
-            default:  // how to i specify nothing?  can i just leave it blank?
+            default:  // how to i specify nothing?  can i just leave it blank?  It didn't complain so I guess so!
         }
-    
         // append the $li to the existing $ul
         
         $ul.append($li);
     }
+
+    // build the table headers.
+    // this is a callback function for use with the headerValues array.
+
+    const buildTableHeader = (headerValue) => {
+
+        // put <th> tags around each column heading
+        let $ColumnHeader = $("<th>").text(headerValue);
+
+        // append column heading to the header row between the <tr> tags.
+
+        $headerRow.append($ColumnHeader);
+        
+        // append this header row to the table.
+        $table.append($headerRow);
+
+        // append it to the container so it shows up on the screen.
+
+        $container.append($table);
+
+    }
+
+
+    // this builds the rows of content for the schedule.
+    // it's the callback function used for the myClasses array.
+
+    const buildSchedule = (element) => {
+
+        // iterate through the myClasses array and build each row of the schedule table.
+
+        let $tableRow = $("<tr>");
+        let $firstColumn = $("<td>").text(element.day);
+        let $secondColumn = $("<td>").text(element.classes);
+
+       // append this to our table with the appropriate row tags around it.
+
+        $tableRow.append($firstColumn, $secondColumn);
+        $table.append($tableRow);
+
+
+    }
+
 
     /** YEAR 1:  Creating an H1 element on the page */
 
@@ -68,6 +115,22 @@ const myStuff = ["butter beer", "invisibility cloak", "magic map", "time turner"
     myStuff.forEach(addToTrunk);
 
     $container.append($ul);
+
+    /** YEAR 4:  Make a table of the classes taken */
+
+    // build table structure
+
+    let $table = $("<table>");
+    let $headerRow = $("<tr>");
+
+    // this function iterates over the table column headings and builds the
+    // <th> pieces.
+
+    tableHeaders.forEach(buildTableHeader);
+
+    // now iterate over the content and build out the remaining pieces of the table.
+
+    myClasses.forEach(buildSchedule);
 
 
   });
