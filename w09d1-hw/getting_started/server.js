aad122b2849559cf041c5cce947b4250733db17c
Tara Young <ty18881@gmail.com>
2020-02-08 23:06:57 -0500

    Tip calculator express application created

--- w09d1-hw/getting_started/server.js
+++ w09d1-hw/getting_started/server.js
@@ -18,6 +18,16 @@ app.get("/greeting/:name", (req, res) => {
     res.send("Greetings and Salutations " + req.params.name + "!");
 });
 
+/**TIP CALCULATOR */
+
+
+app.get("/tip/:checkTotal/:tipPercentage", (req, res) => {
+    let tipAmount = 0;
+    console.log(req.params);
+    tipAmount = parseFloat(req.params.checkTotal) * (parseFloat(req.params.tipPercentage)/100);
+    res.send(`Tip Amount is ${tipAmount} dollars`);
+});
+
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
