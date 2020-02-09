5235ce6ac6940ba57889f4177184e1f802fd0541
Tara Young <ty18881@gmail.com>
2020-02-08 22:52:42 -0500

    Greeting express application created

--- w09d1-hw/getting_started/server.js
+++ w09d1-hw/getting_started/server.js
@@ -8,6 +8,17 @@ const express = require("express");
 /**invoke the function express */
 const app = express();
 
+/** GREETINGS */
+
+app.get("/greeting", (req, res) => {
+    res.send("Generic Greeting!");
+});
+
+app.get("/greeting/:name", (req, res) => {
+    res.send("Greetings and Salutations " + req.params.name + "!");
+});
+
+
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
