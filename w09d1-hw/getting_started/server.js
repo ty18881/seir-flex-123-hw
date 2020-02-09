3786db55bdef971f1c569534297b7833d8f76bab
Tara Young <ty18881@gmail.com>
2020-02-08 23:23:08 -0500

    Magic 8 ball express application created

--- w09d1-hw/getting_started/server.js
+++ w09d1-hw/getting_started/server.js
@@ -28,6 +28,46 @@ app.get("/tip/:checkTotal/:tipPercentage", (req, res) => {
     res.send(`Tip Amount is ${tipAmount} dollars`);
 });
 
+/** Magic 8 Ball */
+let answers = [
+    "It is certain", 
+    "It is decidedly so", 
+    "Without a doubt", 
+    "Yes definitely",
+    "You may rely on it", 
+    "As I see it yes", 
+    "Most likely", 
+    "Outlook good",
+    "Yes", 
+    "Signs point to yes", 
+    "Reply hazy try again", 
+    "Ask again later",
+    "Better not tell you now", 
+    "Cannot predict now", 
+    "Concentrate and ask again",
+    "Don't count on it", 
+    "My reply is no", 
+    "My sources say no",
+    "Outlook not so good", 
+    "Very doubtful"];
+
+
+    // helper function to generate a random integer between 0 and 19.
+    const getRandom =() => {
+        return Math.floor(Math.random() * (19 - 0 + 1) + 0);
+    };
+
+    app.get("/magic/:question", (req, res) => {
+        // return the randomly selected answer to the screen between h1 tags.
+        res.send(`
+    <html>
+        <body>
+            <h2>${req.params.question}</h2>
+            <h1>${answers[getRandom()]}</h1>
+        </body>
+    </html
+    `);
+    });
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
