58427754caaa34e024bb9fd26974b80f346e64d9
Tara Young <ty18881@gmail.com>
2020-02-13 21:23:41 -0500

    Connected my database, can see json in the browser

--- w09d3-hw/pokemon_app/server.js
+++ w09d3-hw/pokemon_app/server.js
@@ -5,11 +5,18 @@ const app = express();
 // PORT
 const port = 3000;
 
+const pokemon = require("./models/pokemon.js");
 
 app.get("/", (req, res) => {
     // res.render("./missions/index.ejs", {allMissions: marsMissions} );
 
     res.send("Welcome to the Pokemon App");
+});
+
+app.get("/pokemon", (req, res) => {
+    // res.render("./missions/index.ejs", {allMissions: marsMissions} );
+
+    res.send(pokemon);
 });
 
 // LISTENER
