054fed408f771c3f8638d7b1c7ea4cbe7e1a2e7f
Tara Young <ty18881@gmail.com>
2020-02-16 19:13:52 -0500

    New route: set amount to negative if user says it is an expense

--- w10d2-hw/budgtr/server.js
+++ w10d2-hw/budgtr/server.js
@@ -4,11 +4,18 @@ const app = express();
 const port = 3000;
 
 // Middleware
-app.use(express.urlencoded({extended: false}));
+app.use( (req, res, next) => {
+    console.log("I run for all routes");
+    // work is done, now continue with the route.
+    next();
+  });
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 // public resources if needed
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // Data source
 
@@ -18,6 +25,35 @@ const spendingData = require("./models/budget.js");
 app.get("/budgets", (req, res) => {
     res.render("index.ejs", {spendingData: spendingData} );
 });
+
+// NEW route - POST to capture and store the data from the NEW route
+app.post("/budgets", (req, res) => {
+
+    // console.log("request body: ", req.body);
+    console.log("request ", req.body);
+
+    // standardize the data being pulled from the form.
+    let newSpending ={};
+    newSpending.date = req.body.date;
+    newSpending.name = req.body.name;
+    newSpending.amount = req.body.amount;
+    
+    newSpending.tags = [];
+    newSpending.tags.push(req.body.tags);
+    
+    newSpending.expense = req.body.expense;
+    
+    // if the item is an expense, set the amount to negative.
+    newSpending.expense == "yes" ? newSpending.amount = 0 - newSpending.amount : newSpending.amount;
+    spendingData.push(newSpending);
+    
+    console.log("updated spending list", spendingData);
+    //  now we want to redirect user to the index route 
+    // by default, redirect sends user to a get route.
+    
+    res.redirect("/budgets");
+    
+  });
 
 // NEW route - renders form to collect the data
 app.get("/budgets/new", (req, res) => {
@@ -29,27 +65,6 @@ app.get("/budgets/:id", (req, res) => {
     res.render("show.ejs", {item: spendingData[req.params.id]} );
 });
 
-// NEW route - POST to capture and store the data from the NEW route
-// app.post("/bakedgoods", (req, res) => {
-//     // res.send("hi");
-//     console.log(req.body);
-    
-  
-//     // standardize the data being pulled from the form.
-//     let newGoodie ={};
-//     newGoodie.name = req.body.name;
-//     newGoodie.price = req.body.price;
-//     newGoodie.image = req.body.image;
-//   // https://images-na.ssl-images-amazon.com/images/I/715J%2BKlb5fL._SX569_.jpg
-//     bakedGoods.push(newGoodie);
-    
-//     console.log("updated goodie list", bakedGoods);
-//     //  now we want to redirect user to the index route 
-//     // by default, redirect sends user to a get route.
-    
-//     res.redirect("/bakedgoods");
-    
-//   });
 
 
 
