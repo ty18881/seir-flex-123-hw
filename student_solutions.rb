27e0f5cb7fe677752bd6761885d33178652f96d9
ty18881 <ty18881@gmail.com>
2020-04-16 23:01:58 -0400

    Hello World done

---
+++ student_solutions.rb
@@ -0,0 +1,14 @@
+## Week 18, Day 2 Homework
+## Ruby Basics
+
+# Hello World
+
+p "Hello World"
+
+adjective = "Big Bad"
+
+p "Hello #{adjective} World"
+
+hello_world = "Hello World"
+
+p hello_world.upcase()
