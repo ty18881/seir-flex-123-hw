539c857564fd1110622cb353dac16590de7353a7
ty18881 <ty18881@gmail.com>
2020-04-25 16:22:18 -0400

    Commit 6/7/8: Can POST new Tweets

--- w20d2/tweeter_app_api/app/controllers/tweets_controller.rb
+++ w20d2/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -9,4 +9,23 @@ class TweetsController < ApplicationController
         found_tweet = Tweet.find(params[:id])
         render json: {status: 200, tweet: found_tweet}
     end
+
+    def create
+        ## creating the new object according the Tweet model
+        ## limiting input to the specified fields.
+        tweet = Tweet.new(tweet_params)
+    ## attempting to save the new tweet into our database.
+    ## return status code and error if applicable.
+        if tweet.save
+          render(status: 201, json: { tweet: tweet }) 
+        else
+          render(status: 422, json: { tweet: tweet, errors: tweet.errors })
+        end
+      end
+
+    private
+    
+    def tweet_params
+        params.require(:tweet).permit(:title, :content, :author)
+    end
 end
