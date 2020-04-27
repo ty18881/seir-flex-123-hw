34b9aa0535bfef02896d7df2276f6a3544e1869c
ty18881 <ty18881@gmail.com>
2020-04-26 20:11:28 -0400

    Refactor code to display pet details in a modal

--- w15d3/sunny_shelter/sunny_shelter_front_end/src/App.js
+++ w15d3/sunny_shelter/sunny_shelter_front_end/src/App.js
@@ -3,6 +3,7 @@ import Popup from "reactjs-popup";
 
 import './App.css';
 import NewForm from "./components/NewForm"
+import ShowAnimal from './components/ShowAnimal';
 
 let baseURL = "http://localhost:3003"
 
@@ -37,29 +38,7 @@ class App extends React.Component {
     });
   };
 
-  toggleAdoptionFlag = input => {
-    // identify the item in the state variable
-    // update the adopted flag
-    // reset state variable
-    // this will trigger a re-rendering i think.
-    console.log("Toggle Adoption Flag - CLICKED");
-    if (input.adopted == true){
-      input.adopted = false;
-      
-    } else
-    {
-      input.adopted = true;
-    }
-    const findIndex = this.state.animals.findIndex(animal => animal._id === input._id)
-    const copyAnimals = [...this.state.animals]
-    copyAnimals.splice(findIndex, 1)
-    // add back the revised version of the animal object.
-    copyAnimals.unshift(input)
-    this.setState({animals: copyAnimals})
-
-    console.log("Toggle Adoption Flag -" + input.adopted)
-  };
-
+ 
 // remove the animal record from the database
 // remove the animal from the collection in state as well so it doesn't appear on the screen.
   deleteAnimal = (id) => {
@@ -78,17 +57,29 @@ class App extends React.Component {
     <div className="container">
         <h1>Sunny Philadelphia Shelter Website</h1>
         <NewForm baseURL={baseURL} handleAddAnimal={this.handleAddAnimal} />
+        <br></br>
         <table>
           <tbody>
             {this.state.animals.map(animal => (
               <tr key={animal._id}>
-                <td> {animal.name}</td>
-                <td> {animal.species}</td>
-                <td> <img src={animal.image} alt="Pet Photo"/></td>
-                <td> {animal.personalityTraits}</td>
-                <td>Adopted? = {animal.adopted == null ? "No": "yes"}</td>
-                <td><button type="button" onClick={() => this.toggleAdoptionFlag(animal)}>Pending Adoption</button></td>
-                <td><button type="button" onClick={() => this.deleteAnimal(animal._id)}>Officially Adopted!</button></td>
+                {/* I want the pet's photo to bring up the modal when clicked */}
+                {/* <td> <img src={animal.image} alt="the here"/>
+                  {animal.name}
+                </td> */}
+                
+                <td> 
+                  <Popup modal 
+                    
+                       trigger={<img src={animal.image} alt="the here"></img> }>
+                         {/* {animal.name} */}
+                  
+                    {close => <ShowAnimal 
+                        animal={animal}
+                        deleteAnimal={this.deleteAnimal}
+                        baseURL={baseURL}
+                        />}
+                  </Popup>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -96,6 +87,8 @@ class App extends React.Component {
       </div>
   );
 }
+
+// trigger={<button>Click to learn more about {animal.name}</button>}>
 
 componentDidMount() {
   this.getAnimals();
