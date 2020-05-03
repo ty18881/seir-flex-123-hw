242dbca41282d7887264d2cbaa2a10e9ab0b1252
ty18881 <ty18881@gmail.com>
2020-05-03 17:55:39 -0400

    Commit 3: React Module and Controller

--- w20d3-hw/dating_app/dating_app_client/src/components/Hello.js
+++ w20d3-hw/dating_app/dating_app_client/src/components/Hello.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class Daters extends Component {
+
+    // add method to collect Daters from our database
+
+    getDaters = () => {
+        fetch("http://localhost:3000/users")
+        .then( daters => console.log(daters))
+    };
+
+    componentDidMount() {
+        this.getDaters();
+    }
     render () {
         return (
             <h1>Hello Dating World!</h1>
