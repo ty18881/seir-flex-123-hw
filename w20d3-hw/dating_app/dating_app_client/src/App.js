26545b61b7b8a4edb6467cf9746b39a2df991adc
ty18881 <ty18881@gmail.com>
2020-05-03 19:00:15 -0400

    Commit 5: Flexbox/Grid Layout

--- w20d3-hw/dating_app/dating_app_client/src/App.js
+++ w20d3-hw/dating_app/dating_app_client/src/App.js
@@ -7,6 +7,18 @@ class App extends Component {
   render () {
   return (
     <div className="App">
+      <div className="HolyGrail">
+        <header>YOUR HEADER HERE</header>
+        <div className="HolyGrail-body"> 
+
+            <main className="HolyGrail-content"><h2>The Big Open Space in the Center</h2></main>
+            <nav className="HolyGrail-nav"><h2>The Left Hand Side Pane</h2></nav>
+            <aside className="HolyGrail-ads"><h2>The Right Hand Side Pane</h2></aside>
+          </div>
+
+        <footer><h2>YOUR FOOTER HERE - Eventually a form</h2></footer>
+        
+      </div>
      <Daters />
     </div>
   );
--- w20d3-hw/dating_app/dating_app_client/src/components/Hello.js
+++ w20d3-hw/dating_app/dating_app_client/src/components/Hello.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 
 class Daters extends Component {
 
+    // hold daters in state
+    state = {
+        daters: []
+    }
+
     // add method to collect Daters from our database
+    // and store the collection in state
 
     getDaters = () => {
         fetch("http://localhost:3000/users")
-        .then( daters => console.log(daters))
+        .then(res => res.json())
+        .then( jDaters => this.setState({ daters: jDaters}))
+        .catch( error => console.log(error))
     };
 
     componentDidMount() {
         this.getDaters();
     }
+
     render () {
         return (
             <h1>Hello Dating World!</h1>
