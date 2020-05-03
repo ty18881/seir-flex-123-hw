856004d6465250a287ac3be9a8b2725d180d296b
ty18881 <ty18881@gmail.com>
2020-05-03 15:41:51 -0400

    Commit 2: Made React App

---
+++ w20d3-hw/dating_app/dating_app_client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Daters from "./components/Hello.js"
+
+class App extends Component {
+  render () {
+  return (
+    <div className="App">
+     <Daters />
+    </div>
+  );
+}
+}
+
+export default App;
